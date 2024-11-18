98044a205051309f4cc4bf760a81a5bd5dfbc31b
varshahumbe <infovarshahumbe@gmail.com>
2024-11-18 10:54:25 +0530

    Update ubuntu-minikube.sh

--- ubuntu-minikube.sh
+++ ubuntu-minikube.sh
@@ -1,12 +1,19 @@
+#update server
 sudo apt update -y
 sudo apt upgrade -y
+
+#install Docker
 sudo apt install curl wget apt-transport-https -y
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+#INSTALLMINIKUBE 
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 sudo chmod +x /usr/local/bin/minikube
 sudo minikube version
+
+#INSTALL KUBECTL
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
